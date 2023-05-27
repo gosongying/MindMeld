d86a2d86b8ceb9be07306c811f983d063df9da20
GO SONG YING <122217734+gosongying@users.noreply.github.com>
2023-05-27 14:28:18 +0800

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import LandingPage from './src/screen/LandingPage'
 import LoginPage from './src/screen/LoginPage'
 import SignupPage from './src/screen/SignupPage'
 import HomePage from './src/screen/HomePage'
+import About from './src/screen/settingScreen/About'
+import Privacy from './src/screen/settingScreen/Privacy'
+import Profile from './src/screen/settingScreen/Profile'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import CarouselCard from './src/components/CarouselCard.js'
