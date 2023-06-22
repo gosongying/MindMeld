c1a94343f580deefb1f1cc524eb095f83c5abf9f
Janssen <e0969027@u.nus.edu>
2023-06-22 15:08:16 +0800

    Bug fix home page

--- src/screen/Home/HomePage.js
+++ src/screen/Home/HomePage.js
@@ -11,7 +11,6 @@ import { update, ref, onDisconnect, serverTimestamp, goOnline, onValue, incremen
 import { database, auth } from '../../../firebase';
 
 const HomePage = () => {
-
   const currentUser = auth.currentUser;
 
   console.log("Home");
@@ -20,13 +19,13 @@ const HomePage = () => {
     const userIdRef = ref(database, 'userId/' + currentUser.uid);
     try {
       if (!currentUser.isAnonymous) {
-         //to handle user online status when user close the app suddenly
-        onDisconnect(userIdRef).update({status: increment(-1)});
-        //to handle user ongoing sessions when user close the app suddenly
+        // to handle user online status when user closes the app suddenly
+        onDisconnect(userIdRef).update({ status: increment(-1) });
+        // to handle user ongoing sessions when user closes the app suddenly
         onDisconnect(userIdRef).update({
           ongoingSessions: null
         });
-        //use runTransaction to update the number of online account correctly
+        // use runTransaction to update the number of online account correctly
         runTransaction(userIdRef, (profile) => {
           if (profile) {
             profile.status++;
@@ -35,57 +34,99 @@ const HomePage = () => {
             return profile;
           }
         });
-        } else {
-          //if anonymous user, then delete the account
-          onDisconnect(userIdRef).remove();
-        }
-    } catch(error) {
+      } else {
+        // if anonymous user, then delete the account
+        onDisconnect(userIdRef).remove();
+      }
+    } catch (error) {
       console.log(error);
     }
   }, []);
 
   const Tab = createBottomTabNavigator();
 
-    return (
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-  
-            if (route.name === 'StudyDashboardTab') {
-              iconName = focused ? 'book' : 'book-outline';
-            } else if (route.name === 'StudyFeedsTab') {
-              iconName = focused ? 'people' : 'people-outline';
-            } else if (route.name === 'AchievementsTab') {
-              iconName = focused ? 'trophy' : 'trophy-outline';
-            } else if (route.name === 'SettingTab') {
-              iconName = focused ? 'menu' : 'menu-outline';
-            } else if (route.name === 'StudySessionTab') {
-              iconName = focused ? 'easel' : 'easel-outline'
-            }
-  
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: '#710EF1',
-          tabBarInactiveTintColor: 'gray',
-          tabBarStyle: {
-            backgroundColor: '#ffffff',
-          },
-          tabBarLabelStyle: {
-            textTransform: 'none',
-          },
-          tabBarIndicatorStyle: {
-            backgroundColor: '#710ef1',
-          },
-        })}>
-        <Tab.Screen name={'StudyDashboardTab'} component={StudyDashboardTab} options={{ tabBarLabel: 'Study', headerShown: false }} />
-        <Tab.Screen name={'StudySessionTab'} component={StudySessionTab} options={{ tabBarLabel: 'Session', headerShown: false }} />
-        {!currentUser.isAnonymous && <Tab.Screen name={'StudyFeedsTab'} component={StudyFeedsTab} options={{ tabBarLabel: 'Feeds', headerShown: false }} />}
-        {!currentUser.isAnonymous && <Tab.Screen name={'AchievementsTab'} component={AchievementTab} options={{ tabBarLabel: 'Achievement', headerShown: false }} />}
-        <Tab.Screen name={'SettingTab'} component={SettingTab} options={{ tabBarLabel: 'Setting' ,headerShown: false }} />
-      </Tab.Navigator>
-    );
-  };
-  
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarButton: ({ onPress, accessibilityState }) => {
+          const tabName =
+            route.name === 'StudyDashboardTab' ? 'Study'
+            : route.name === 'StudySessionTab' ? 'Session'
+            : route.name === 'StudyFeedsTab' ? 'Feeds'
+            : route.name === 'AchievementsTab' ? 'Achievement'
+            : route.name === 'SettingTab' ? 'Setting' : '';
 
-export default HomePage
+          if (accessibilityState.selected) {
+            return (
+              <Pressable
+                style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                accessibilityRole="button"
+                accessibilityState={{ disabled: true }}
+              >
+                <Ionicons
+                  name={route.name === 'StudyDashboardTab' ? 'book' :
+                    route.name === 'StudyFeedsTab' ? 'people' :
+                    route.name === 'AchievementsTab' ? 'trophy' :
+                    route.name === 'SettingTab' ? 'menu' :
+                    route.name === 'StudySessionTab' ? 'easel' : 'book-outline'}
+                  size={25}
+                  color={'#710EF1'}
+                />
+                <Text style={{ color: '#710EF1', fontSize: 10 }}>{tabName}</Text>
+              </Pressable>
+            );
+          } else {
+            return (
+              <Pressable
+                style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                onPress={onPress}
+                accessibilityRole="button"
+              >
+                <Ionicons
+                  name={route.name === 'StudyDashboardTab' ? 'book'
+                    : route.name === 'StudyFeedsTab' ? 'people'
+                    : route.name === 'AchievementsTab' ? 'trophy'
+                    : route.name === 'SettingTab' ? 'menu'
+                    : route.name === 'StudySessionTab' ? 'easel' : 'book-outline'}
+                  size={25}
+                  color={'gray'}
+                />
+                <Text style={{ color: 'gray', fontSize: 10 }}>{tabName}</Text>
+              </Pressable>
+            );
+          }
+        },
+        tabBarIcon: ({ focused }) => {
+          return <Ionicons
+            name={route.name === 'StudyDashboardTab' ? focused ? 'book' : 'book-outline'
+              : route.name === 'StudyFeedsTab' ? focused ? 'people' : 'people-outline'
+              : route.name === 'AchievementsTab' ? focused ? 'trophy' : 'trophy-outline'
+              : route.name === 'SettingTab' ? focused ? 'menu' : 'menu-outline'
+              : route.name === 'StudySessionTab' ? focused ? 'easel' : 'easel-outline' : 'book-outline'}
+            size={25}
+            color={focused ? '#710EF1' : 'gray'}
+          />;
+        },
+        tabBarActiveTintColor: '#710EF1',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          backgroundColor: '#ffffff',
+        },
+        tabBarLabelStyle: {
+          textTransform: 'none',
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: '#710ef1',
+        },
+      })}
+    >
+      <Tab.Screen name={'StudyDashboardTab'} component={StudyDashboardTab} options={{ tabBarLabel: 'Study', headerShown: false }} />
+      {!currentUser.isAnonymous && <Tab.Screen name={'StudySessionTab'} component={StudySessionTab} options={{ tabBarLabel: 'Session', headerShown: false }} />}
+      <Tab.Screen name={'StudyFeedsTab'} component={StudyFeedsTab} options={{ tabBarLabel: 'Feeds', headerShown: false }} />
+      {!currentUser.isAnonymous && <Tab.Screen name={'AchievementsTab'} component={AchievementTab} options={{ tabBarLabel: 'Achievement', headerShown: false }} />}
+      <Tab.Screen name={'SettingTab'} component={SettingTab} options={{ tabBarLabel: 'Setting', headerShown: false }} />
+    </Tab.Navigator>
+  );
+};
+
+export default HomePage;
